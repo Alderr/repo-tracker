7c8bb80d2cefce7cdb71eef2e1c43a9685e0d01f
Vernon Mensah <Alderr@users.noreply.github.com>
2020-09-29 22:55:20 -0400

    add params n some logic

--- src/API/Github.js
+++ src/API/Github.js
@@ -1,14 +1,18 @@
 import { Octokit } from '@octokit/rest';
 const octokit = new Octokit();
 
-export function searchRepos() {
-  return octokit.search.repos();
+export function searchRepos({ sort, order, term }) {
+  return octokit.search.repos({
+    q: term,
+    sort,
+    order,
+  });
 }
 
-export function getRepo() {
-  return octokit.repos.get();
+export function getRepo({ owner, repo }) {
+  return octokit.repos.get({ owner, repo });
 }
 
-export function listReleases() {
-  return octokit.repos.listReleases();
+export function listReleases({ owner, repo }) {
+  return octokit.repos.listReleases({ owner, repo });
 }
--- src/API/Repo.js
+++ src/API/Repo.js
@@ -2,12 +2,28 @@ export function getRepo({ id }) {
   return JSON.stringify(localStorage.getItem(id));
 }
 
-export function addRepo({ id, latestRelease, userRelease }) {
-  return localStorage.setItem(id, JSON.stringify({ id, latestRelease, userRelease }));
+export function addRepo({
+  id, latestRelease, userRelease, owner, repo,
+}) {
+  return localStorage.setItem(id, JSON.stringify({
+    id, latestRelease, userRelease, owner, repo,
+  }));
 }
 
-export function updateRepo({ id, latestRelease, userRelease }) {
-  return localStorage.setItem(id, JSON.stringify({ id, latestRelease, userRelease }));
+export function updateRepo({
+  id, latestRelease, userRelease, owner, repo,
+}) {
+  const prevRepo = JSON.parse(localStorage.getItem(id));
+
+  return localStorage.setItem(id, JSON.stringify(
+    {
+      ...prevRepo,
+      ...latestRelease ? { latestRelease } : {},
+      ...userRelease ? { userRelease } : {},
+      ...owner ? { owner } : {},
+      ...repo ? { repo } : {},
+    },
+  ));
 }
 
 export function deleteRepo({ id }) {
